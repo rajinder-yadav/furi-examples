530a76807a439ee7cc9e708e48b854a1f5c052d3
Dev Guy <devguy.ca@gmail.com>
2025-03-15 00:44:37 -0400

    refactor: access Cors off Furi

--- 15-cors-middleware/main.ts
+++ 15-cors-middleware/main.ts
@@ -21,19 +21,19 @@
  * credentials?: boolean;                                 *
  * maxAge?: number;                                       *
  **********************************************************/
-import { Furi, ApplicationContext, Cors } from '@furi-server/furi';
+import { Furi, ApplicationContext } from '@furi-server/furi';
 
 const app = new Furi();
 
 // The most basic way to enable CORS.
 // This will enable for all '*' origins.
 //
-// app.use(Cors());
+// app.use(Furi.Cors());
 
 // Passing an options object to enable CORS.
 // NOTE: A single origin can be specified as a string.
 //
-// app.use(Cors(
+// app.use(Furi.Cors(
 //   {
 //     origin: '*',
 //     methods: ['GET','OPTIONS','HEAD','PUT','PATCH','POST','DELETE'],
@@ -47,7 +47,7 @@ const app = new Furi();
 // NOTE: Multiple origins must be specified as an array.
 // You do not need to specift all the option properties. Only the ones you need.
 //
-app.use(Cors(
+app.use(Furi.Cors(
   {
     origin: ['http://localhost:3000', 'http://localhost:3001'],
     methods: ['GET', 'OPTIONS', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
